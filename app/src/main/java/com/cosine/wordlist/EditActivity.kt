22f52bc60e16d73fc99a14c6ead8f62dc724d114
CosineAA <rhdwlgns0725@gmail.com>
2022-11-03 15:58:44 +0900

    update

--- app/src/main/java/com/cosine/wordlist/EditActivity.kt
+++ app/src/main/java/com/cosine/wordlist/EditActivity.kt
@@ -1,49 +1,88 @@
 package com.cosine.wordlist
 
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.text.Editable
 import android.view.View
 import android.widget.EditText
 import android.widget.LinearLayout
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.children
 import androidx.core.view.size
+import com.cosine.wordlist.MainActivity.Companion.nowCategory
+import com.cosine.wordlist.MainActivity.Companion.setting
+import com.cosine.wordlist.databinding.ActivityMainBinding
+import com.cosine.wordlist.databinding.ContentsEditModeBinding
 import com.cosine.wordlist.databinding.WordAndAnswerBinding
-import com.wajahatkarim3.easyflipview.EasyFlipView
+import org.json.JSONArray
+import org.json.JSONObject
 
 class EditActivity : AppCompatActivity() {
 
-    private lateinit var wordList: LinearLayout
+    private lateinit var editBinding: ContentsEditModeBinding
+
+    private lateinit var preferences: SharedPreferences
+    private lateinit var editor: SharedPreferences.Editor
+
     private lateinit var wordListEdit: LinearLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.contents_edit_mode)
-//        wordListEdit = findViewById(R.id.wordListEdit)
-//        wordList = findViewById(R.id.wordList)
-    }
+        editBinding = ContentsEditModeBinding.inflate(layoutInflater)
+        setContentView(editBinding.root)
 
+        wordListEdit = editBinding.wordListEdit
+
+        preferences = getSharedPreferences("Data", MODE_PRIVATE)
+        editor = preferences.edit()
+        loadWordData()
+    }
     override fun onStop() {
         super.onStop()
-
-//        val wordCount = wordListEdit.childCount - 1
-//
-//        if (wordCount == 0) return
-
-//        for (loop in 0..wordCount) {
-//            val wordAndAnswer = wordListEdit.getChildAt(loop)
-//
-//            if (wordAndAnswer is LinearLayout) {
-//                val word = (wordAndAnswer.getChildAt(0) as EditText).text.toString()
-//                val answer = (wordAndAnswer.getChildAt(1) as EditText).text.toString()
-//                if (word.isEmpty() || answer.isEmpty()) continue
-//                MainActivity.editor.putString(word, answer)
-//                MainActivity.editor.apply()
-//            }
-//        }
+        saveWordData()
+        setting = false
     }
-    fun setWordListToEditor() {
+    private fun loadWordData() {
+        val jsonString = preferences.getString("$nowCategory", "0") ?: return
+        val categoryArray = JSONObject(jsonString)
 
+        for (word in categoryArray.keys()) {
+            val answer = categoryArray.getString(word)
+            addWordSetting(word, answer)
+        }
+    }
+    private fun addWordSetting(word: String, answer: String) {
+        val index = wordListEdit.size - 1
+        val newWordCard = WordAndAnswerBinding.inflate(layoutInflater)
+        val wordEdit = newWordCard.wordEdit
+        val answerEdit = newWordCard.answerEdit
+        wordEdit.text = word.toEditable()
+        answerEdit.text = answer.toEditable()
+
+        val layoutParams = LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.MATCH_PARENT,
+            LinearLayout.LayoutParams.WRAP_CONTENT
+        )
+        layoutParams.setMargins(30, 0, 0, 60)
+
+        wordListEdit.addView(newWordCard.wordAndAnswerEdit, index, layoutParams)
+    }
+    private fun saveWordData() {
+        val wordCount = wordListEdit.childCount - 1
+        if (wordCount == 0) return
+
+        val categoryArray = JSONObject()
+        for (loop in 0..wordCount) {
+            val wordAndAnswer = wordListEdit.getChildAt(loop)
+            if (wordAndAnswer is LinearLayout) {
+                val word = (wordAndAnswer.getChildAt(0) as EditText).text.toString()
+                val answer = (wordAndAnswer.getChildAt(1) as EditText).text.toString()
+                if (word.isEmpty() || answer.isEmpty()) continue
+                categoryArray.put(word, answer)
+            }
+        }
+        editor.putString("$nowCategory", categoryArray.toString())
+        editor.apply()
     }
     fun onClickWordPlusButton(view: View) {
         val index = wordListEdit.size - 1
--- app/src/main/java/com/cosine/wordlist/MainActivity.kt
+++ app/src/main/java/com/cosine/wordlist/MainActivity.kt
@@ -1,40 +1,109 @@
 package com.cosine.wordlist
 
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
+import android.view.View.OnClickListener
+import android.widget.ImageButton
 import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
 import androidx.core.view.children
 import androidx.drawerlayout.widget.DrawerLayout
+import com.cosine.wordlist.databinding.ActivityMainBinding
+import com.cosine.wordlist.databinding.WordCardBinding
 import com.google.android.material.navigation.NavigationView
+import kotlinx.coroutines.*
+import org.json.JSONObject
 
 
-class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+class MainActivity : AppCompatActivity(),
+    NavigationView.OnNavigationItemSelectedListener, OnClickListener {
+
+    companion object {
+        val scope = CoroutineScope(Dispatchers.Main)
+        var nowCategory: Int = 0
+        var setting = false
+    }
+
+    private lateinit var mainBinding: ActivityMainBinding
+
+    private lateinit var preferences: SharedPreferences
+    private lateinit var editor: SharedPreferences.Editor
 
     private lateinit var navigationView: NavigationView
     private lateinit var drawerLayout: DrawerLayout
-    private lateinit var wordList: LinearLayout
+    private lateinit var editButton: ImageButton
 
-    private lateinit var defaultCategory: DefaultCategory
-    private lateinit var otherCategory: OtherCategory
+    private lateinit var categoryWordList0: LinearLayout
+    private lateinit var categoryWordList1: LinearLayout
+    private lateinit var categoryWordList2: LinearLayout
+    private lateinit var categoryWordList3: LinearLayout
+    private lateinit var categoryWordList4: LinearLayout
+    private lateinit var categoryWordList5: LinearLayout
+    private lateinit var categoryWordList6: LinearLayout
+    private lateinit var categoryWordList7: LinearLayout
+    private lateinit var categoryWordList8: LinearLayout
+    private lateinit var categoryWordList9: LinearLayout
+    private lateinit var categoryWordList10: LinearLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainBinding.root)
 
-        navigationView = findViewById(R.id.categoryNavigation)
-        drawerLayout = findViewById(R.id.drawerLayout)
-        //wordList = findViewById(R.id.wordList)
+        preferences = getSharedPreferences("Data", MODE_PRIVATE)
+        editor = preferences.edit()
 
-        defaultCategory = DefaultCategory()
-        otherCategory = OtherCategory()
-        supportFragmentManager.beginTransaction().add(com.google.android.material.R.id.container, defaultCategory).commit()
+        navigationView = mainBinding.categoryNavigation
+        drawerLayout = mainBinding.drawerLayout
+        editButton = mainBinding.category.editModeButton
+
+        categoryWordList0 = mainBinding.category.category0.wordList
+        categoryWordList1 = mainBinding.category.category1.wordList
+        categoryWordList2 = mainBinding.category.category2.wordList
+        categoryWordList3 = mainBinding.category.category3.wordList
+        categoryWordList4 = mainBinding.category.category4.wordList
+        categoryWordList5 = mainBinding.category.category5.wordList
+        categoryWordList6 = mainBinding.category.category6.wordList
+        categoryWordList7 = mainBinding.category.category7.wordList
+        categoryWordList8 = mainBinding.category.category8.wordList
+        categoryWordList9 = mainBinding.category.category9.wordList
+        categoryWordList10 = mainBinding.category.category10.wordList
 
         navigationView.setNavigationItemSelectedListener(this)
+        editButton.setOnClickListener(this)
+
+        loadWordData()
+    }
+    private fun loadWordData() {
+        categoryWordList0.removeAllViews()
+        val jsonString = preferences.getString("$nowCategory", null) ?: return
+        val categoryArray = JSONObject(jsonString)
+
+        for (word in categoryArray.keys()) {
+            val answer = categoryArray.getString(word)
+            addWordCard(word, answer)
+        }
+    }
+    private fun addWordCard(word: String, answer: String) {
+        val newWordCard = WordCardBinding.inflate(layoutInflater)
+
+        newWordCard.apply {
+            wordBack.root.text = answer.toEditable()
+            wordFront.root.text = word.toEditable()
+        }
+
+        val layoutParams = LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.MATCH_PARENT,
+            LinearLayout.LayoutParams.WRAP_CONTENT
+        )
+        layoutParams.setMargins(0, 0, 0, 60)
+
+        categoryWordList0.addView(newWordCard.wordEdit, layoutParams)
     }
     override fun onBackPressed() {
         if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
@@ -43,48 +112,61 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
             super.onBackPressed()
         }
     }
-
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         val menu = navigationView.menu
         val categories = menu.children
         val last = categories.indexOf(categories.last())
-        when (val order = categories.indexOf(item)) {
-            last -> {
-                menu.add(0, R.id.categoryItem, 1, "1")
-            }
-            else -> {
-                onChangedFragment(order)
-            }
-            //            if (drawerLayout.isDrawerOpen(GravityCompat.START))
-            //                drawerLayout.closeDrawer(GravityCompat.START)
+        val order = categories.indexOf(item)
+        nowCategory = order
+        when (order) {
+            0 -> onChangeCategory(categoryWordList0)
+            1 -> onChangeCategory(categoryWordList1)
+            2 -> onChangeCategory(categoryWordList2)
+            3 -> onChangeCategory(categoryWordList3)
+            4 -> onChangeCategory(categoryWordList4)
+            5 -> onChangeCategory(categoryWordList5)
+            6 -> onChangeCategory(categoryWordList6)
+            7 -> onChangeCategory(categoryWordList7)
+            8 -> onChangeCategory(categoryWordList8)
+            9 -> onChangeCategory(categoryWordList9)
+            10 -> onChangeCategory(categoryWordList10)
         }
+        if (drawerLayout.isDrawerOpen(GravityCompat.START))
+            drawerLayout.closeDrawer(GravityCompat.START)
         return true
     }
-    fun onChangedFragment(order: Int) {
-        supportFragmentManager.beginTransaction().replace(com.google.android.material.R.id.container, defaultCategory).commit()
+    private fun onChangeCategory(targetCategory: LinearLayout) {
+        mainBinding.category.root.children.forEach {
+            println("뷰: $it")
+            if (it is LinearLayout) {
+                if (it != targetCategory) {
+                    it.visibility = View.INVISIBLE
+                }
+            }
+        }
     }
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.categories, menu)
         return true
     }
-    fun onClickEditModeButton(view: View) {
+
+    override fun onClick(p0: View?) {
         val intent = Intent(applicationContext, EditActivity::class.java)
         startActivity(intent)
+
+        setting = true
+        scope.launch {
+            while (this.isActive) {
+                delay(100)
+                if (!setting) {
+                    this.cancel()
+                    loadWordData()
+                }
+            }
+        }
     }
-//    fun addWordCard(word: String, answer: String) {
-//        val newWordCard = WordCardBinding.inflate(layoutInflater)
-//
-//        newWordCard.apply {
-//            this.wordBack.root.text = answer.toEditable()
-//            this.wordFront.root.text = word.toEditable()
-//        }
-//
-//        val layoutParams = LinearLayout.LayoutParams(
-//            LinearLayout.LayoutParams.MATCH_PARENT,
-//            LinearLayout.LayoutParams.WRAP_CONTENT
-//        )
-//        layoutParams.setMargins(0, 0, 0, 60)
-//
-//        wordList.addView(newWordCard.wordEdit, layoutParams)
-//    }
+    override fun onDestroy() {
+        super.onDestroy()
+        scope.cancel()
+    }
 }
